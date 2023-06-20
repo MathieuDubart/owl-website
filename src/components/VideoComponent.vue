b6d63405b41232d6c2e3ebaf0a42acfb04a0cd4e
Mathieu Dubart <mathieudubart@mini-de-mathieu.home>
2023-06-20 18:05:13 +0200

    changing vimeo video

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="col-md-12 vimeo-integration">
-    <iframe src="https://player.vimeo.com/video/329317115?h=1a36fe4486&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+    <iframe src="https://player.vimeo.com/video/837987013?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Owl commercial"></iframe>
   </div>
 </template>
